ead1abb70341b4ed619caa9c191ba5a09ff4f79c
deadprogram <ron@hybridgroup.com>
2015-05-09 11:34:49 -0700

    Initial commit

---
+++ test/specs/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Devicehive = source("adaptor");
+
+describe("Cylon.Adaptors.Devicehive", function() {
+  var adaptor = new Devicehive();
+
+  it("needs tests");
+});
---
+++ test/specs/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Devicehive = source("driver");
+
+describe("Cylon.Drivers.Devicehive", function() {
+  var driver = new Devicehive({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
